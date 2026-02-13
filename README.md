81e7ca150e7aebd0d5578039ac2ebadd3719a253
Raghuvver <raghuveersinghrathore245@gmail.com>
2026-02-13 23:39:09 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # namaste-react
+# Parcel 
+- Dev Build
+- Local server 
+- HMR = Hot module Replacement
+- File Watching Algorithm - written in cpp
+- Caching - faster Builds
+- Image optimization
+- Minification 
+- Bundling
+- Compressing 
+- Consistent Hasing 
+- Code spliting 
+- Differentail Bundling -support older browesers 
+- Diagnostics
+- Error handling
+- HTTPS
+- Tree Shaking - remove unused code 
---
+++ app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent"}, [
+  React.createElement("div", {id: "child", key: "child1"}, [
+    React.createElement("h1", {key: "h1-1"}, "Im an h1 tag"),
+    React.createElement("h2", {key: "h2-1"}, "Im an h2 tag"),
+  ]),
+  React.createElement("div", {id: "child2", key: "child2"}, [
+    React.createElement("h1", {key: "h1-2"}, "Im an h1 tag"),
+    React.createElement("h2", {key: "h2-2"}, "Im an h2 tag"),
+  ]),
+]);
+
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
